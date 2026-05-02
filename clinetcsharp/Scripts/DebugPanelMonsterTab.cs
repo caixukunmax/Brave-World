1d9439f394a7bf1f8d8f3d41367024298d14acbd
Qoder <qoder@tslua2.com>
2026-05-02 19:41:24 +0800

    debug: add MP LengthScale logging to MonsterTab

--- clinetcsharp/Scripts/DebugPanelMonsterTab.cs
+++ clinetcsharp/Scripts/DebugPanelMonsterTab.cs
@@ -244,7 +244,10 @@ namespace ClinetCSharp
 
             _monsterMpBarVisibleCheck.Toggled += _ => ApplyStyleChanges();
             _monsterMpBarColorBtn.Pressed += OnMonsterMpBarColorPressed;
-            _monsterMpBarLengthScaleSlider.ValueChanged += _ => ApplyStyleChanges();
+            _monsterMpBarLengthScaleSlider.ValueChanged += v => {
+                GD.Print($"[MonsterTab] MP LengthScale slider changed: {v:F4}");
+                ApplyStyleChanges();
+            };
             AttachValueLineEdit(_monsterMpBarLengthScaleSlider, _monsterMpBarLengthScaleValue);
             _monsterMpBarHeightScaleSlider.ValueChanged += _ => ApplyStyleChanges();
             AttachValueLineEdit(_monsterMpBarHeightScaleSlider, _monsterMpBarHeightScaleValue);
@@ -270,6 +273,7 @@ namespace ClinetCSharp
             cfg.HpBarOffsetY = (float)_monsterHpBarOffsetYSlider.Value;
             cfg.MpBarVisible = _monsterMpBarVisibleCheck.ButtonPressed;
             cfg.MpBarLengthScale = (float)_monsterMpBarLengthScaleSlider.Value;
+            GD.Print($"[MonsterTab] ApplySubclassChanges: MpBarLengthScale={cfg.MpBarLengthScale:F4}");
             cfg.MpBarHeightScale = (float)_monsterMpBarHeightScaleSlider.Value;
             cfg.MpBarFillPercent = (float)(_monsterMpBarFillSlider.Value / 100.0);
             cfg.MpBarOffsetX = _monsterMpBarOffsetXCenterCheck?.ButtonPressed == true ? 0 : (float)_monsterMpBarOffsetXSlider.Value;
